e2f93318e6492b6ba7c1d2c35c6d8026a2703bd0
ghornig <gjhornig1@gmail.com>
2025-03-12 20:10:17 -0600

    can get temp updates

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,31 @@
-from flask import render_template
+from flask import Flask, render_template, Response
+import time
 from app import app
+import random
+
+# app = Flask(__name__)
+
+@app.route('/content') # render the content a url differnt from index. This will be streamed into the iframe
+def content():
+    def timer(t):
+        for i in range(t):
+            time.sleep(1) #put 60 here if you want to have seconds
+            yield str(i)
+    return Response(timer(10), mimetype='text/html') #at the moment the time value is hardcoded in the function just for simplicity
+
+@app.get("/temperature/")
+def temp():
+    tempval = random.uniform(20, 30)
+
+    #Do anything here...
+    #Here i'm returning the current value of count, so you can get
+    #It in you web site
+    return str(tempval)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('test.html') # render a template at the index. The content will be embedded in this template
+
+# if __name__ == '__main__':
+#     app.run(use_reloader=False)
